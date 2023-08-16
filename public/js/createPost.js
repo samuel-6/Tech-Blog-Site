5ef6b7910431084d137dd79b426f95cebf8dd37a
Azura <saamueel64@outlook.es>
2023-08-16 17:47:03 -0600

    Updated the script for creating posts.

--- public/js/createPost.js
+++ public/js/createPost.js
@@ -1,10 +1,13 @@
+// This script handles the creation of new posts.
 async function createPostHandler(event) {
 
+    // Prevents the default form action from being carried out.
     event.preventDefault();
 
+    // Redirects the user to the new post page.
     document.location.replace('/dashboard/new')
     
 }
 
-
+// Adds an event listener to the create new post button.
 document.querySelector('#create-new-post').addEventListener('click', createPostHandler);
