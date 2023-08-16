f7d77a397adce6a9e54730cd40078e54fd52f492
Azura <saamueel64@outlook.es>
2023-08-16 12:05:31 -0600

    Created the connections file to handle the connection to the db.

---
+++ config/connections.js
@@ -0,0 +1,35 @@
+// Purpose: Establishes connection to database.
+
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+// If the app is deployed on Heroku, use the JawsDB database.
+if (process.env.JAWSDB_URL) {
+
+    sequelize = new Sequelize (process.env.JAWSDB_URL);
+
+}
+
+// Otherwise, use the local database.
+else {
+
+    sequelize = new Sequelize (
+
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+
+            host: '127.0.0.1',
+            dialect: 'mysql',
+            port: 3306
+
+        }
+
+    );
+
+}
+
+module.exports = sequelize;
