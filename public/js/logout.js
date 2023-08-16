06a46ceeb61e7a4ba9b6c0bd260e49a64327d95b
Azura <saamueel64@outlook.es>
2023-08-16 17:12:08 -0600

    Modified the logout script and added comments.

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,25 +1,32 @@
+// Defines the logout function.
 async function logout() {
 
+    // Sends a fetch request to the logout route.
     const response = await fetch('/api/users/logout', {
 
-        method: 'post',
+        method: 'post', // Specifies that the request will be a POST request.
         headers: {
 
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json' // Specifies that the request body will be JSON.
 
         }
 
     });
 
+    // If the response is ok, the user is redirected to the homepage.
     if (response.ok) {
 
         document.location.replace('/');
 
-    } else {
+    } 
+    
+    // Otherwise, displays an alert with the response status text.
+    else {
 
         alert(response.statusText);
         
     }
 }
 
+// Adds an event listener to the logout button.
 document.querySelector('#logout').addEventListener('click', logout);
