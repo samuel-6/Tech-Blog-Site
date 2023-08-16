4d179cbfef9173304f1d21a460bafabbf19e6be8
Azura <saamueel64@outlook.es>
2023-08-16 17:54:51 -0600

    Updated the sign up script.

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,10 +1,14 @@
+// This script handles the signup functionality.
 async function signupFormHandler(event) {
 
+    // Prevents the default form action from being carried out.
     event.preventDefault();
 
+    // Gets the values from the signup form.
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
+    // If both values are present, sends a fetch request to the users route.
     if (username && password) {
 
         const response = await fetch('/api/users', {
@@ -18,16 +22,20 @@ async function signupFormHandler(event) {
             }),
             headers: {
 
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json' // Specifies that the request body will be JSON.
 
             }
         });
 
+        // If the response is ok, the user is redirected to the dashboard.
         if (response.ok) {
 
             document.location.replace('/dashboard');
 
-        } else {
+        }
+        
+        // Otherwise, displays an alert with the response status text.
+        else {
 
             alert(response.statusText);
             
@@ -35,4 +43,5 @@ async function signupFormHandler(event) {
     }
 }
 
+// Adds an event listener to the signup form.
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
