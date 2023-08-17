89aedb30a207ceb7a37467d15b4f4841b1083227
Azura <saamueel64@outlook.es>
2023-08-16 18:21:39 -0600

    Updated the new post script.

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -1,10 +1,14 @@
+// This script handles the post creation.
 async function newFormHandler(event) {
 
+    // Prevents the default form action from being carried out.
     event.preventDefault();
 
+    // Retrieves the post title and content from the new post form.
     const title = document.querySelector('input[name="post-title"]').value;
     const post_content = document.querySelector('textarea[name="post-content"]').value.trim();
 
+    // Sends a fetch request to create the post.
     const response = await fetch('/api/posts', {
 
         method: 'POST',
@@ -16,17 +20,20 @@ async function newFormHandler(event) {
         }),
         headers: {
 
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json' // Specifies that the request body will be JSON.
 
         }
 
     });
 
+    // If the response is ok, the user is redirected to the dashboard.
     if (response.ok) {
 
         document.location.replace('/dashboard');
 
     }
+    
+    // Otherwise, displays an alert with the response status text.
     else {
 
         alert(response.statusText);
@@ -35,4 +42,5 @@ async function newFormHandler(event) {
 
 }
 
+// Adds an event listener to the new post form.
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
