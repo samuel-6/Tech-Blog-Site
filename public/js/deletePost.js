52f91c36073917304509050eb39a5b8c102d2083
Azura <saamueel64@outlook.es>
2023-08-16 18:16:29 -0600

    updated the delete script.

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -1,28 +1,37 @@
+// This script handles the post deletion.
 async function deleteFormHandler(event) {
 
+    // Prevents the default form action from being carried out.
     event.preventDefault();
 
+    // Extracts the post id from the current url.
     const post_id = window.location.toString().split('/')[
 
         window.location.toString().split('/').length - 1
 
     ];
 
+    // Sends a fetch request to delete the post.
     const response = await fetch(`/api/posts/${post_id}`, {
 
         method: 'DELETE'
 
     });
 
+    // If the response is ok, the user is redirected to the dashboard.
     if (response.ok) {
 
         document.location.replace('/dashboard');
 
-    } else {
+    }
+
+    // Otherwise, displays an alert with the response status text.
+    else {
 
         alert(response.statusText);
         
     }
 }
 
+// Adds an event listener to the delete post button.
 document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
